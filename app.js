90baead1288d36fcb2b35b32c6142ef9ae63f25e
Rohit7000 <69628348+Rohit7000@users.noreply.github.com>
2020-08-13 15:54:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAqHedRile6pUbubTkbcZtGZQ9M6eVJtVk",
+  authDomain: "hacking-b07c6.firebaseapp.com",
+  databaseURL: "https://hacking-b07c6.firebaseio.com",
+  projectId: "hacking-b07c6",
+  storageBucket: "hacking-b07c6.appspot.com",
+  messagingSenderId: "732905466360",
+  appId: "1:732905466360:web:1c0471a5456f674147ce0d",
+  measurementId: "G-05ZYNCTQ47"
 };
-
      
  
 ////Do Not Edit Below This
